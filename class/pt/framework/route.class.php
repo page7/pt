2178fef01da5415f7bbcdcff76171dc72566c7f2
page7 <zhounan0120@gmail.com>
2015-05-21 23:33:57 +0800

    Alpha 0.22 Add Route

---
+++ class/pt/framework/route.class.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * route
+ +-----------------------------------------
+ * @category    pt
+ * @package     pt\framework
+ * @author      page7 <zhounan0120@gmail.com>
+ * @version     $Id$
+ */
+
+namespace pt\framework;
+
+
+class route extends base
+{
+
+
+    static protected $rules = array();
+
+
+    /**
+     * Construct
+     +-----------------------------------------
+     * @access public
+     * @param array $config
+     */
+    public function __construct($config=array())
+    {
+        if (empty($config['routes']))
+            $config['routes'] = array();
+
+        $routes = array_merge($config['routes'], self::$rules);
+
+        $path_info = $_SERVER['PATH_INFO'];
+
+        $count = 0;
+        $reg = str_replace('/', '(\\/', trim($path_info, '/'), $count);
+        $pef = str_repeat('){0,1}', $count+1);
+
+        preg_match_all("/,\/({$reg}{$pef}\//", ','.implode(',', array_keys($routes)), $matchs);
+
+        $matchs = array_filter($matchs[0]);
+        rsort($matchs);
+
+        if (!$matchs) $matchs = array('/');
+
+        foreach ($matchs as $v)
+            if (($r = substr($v, 1)) && isset($routes[$r]) && is_callable($routes[$r]['callback']))
+                return call_user_func_array($routes[$r]['callback'], self::params($path_info, $r, $routes[$r]['params']));
+    }
+
+
+
+
+    /**
+     * Get Params
+     *
+     * @access protected
+     * @param mixed $path_info
+     * @param mixed $route
+     * @return void
+     */
+    static protected function params($path_info, $route, $params)
+    {
+        $_params_val = explode('/', trim(substr($path_info, strlen($route)), '/'));
+
+        if ($params)
+        {
+            $_params_key = explode('/', trim($params, '/'));
+            foreach ($_params_key as $i => $k)
+            {
+                if ($k[0] == '$')
+                $_GET[substr($k, 1)] = $_params_val[$i];
+            }
+        }
+
+        return $_params_val;
+    }
+
+
+
+
+    /**
+     * Add Route Rule
+     +-----------------------------------------
+     * @access public
+     * @param mixed $rule
+     * @param mixed $callback
+     * @return void
+     */
+    static function add($rule, $callback=null, $params=null)
+    {
+        self::$rules[$rule] = array(
+            'params'    => $params,
+            'callback'  => $callback,
+        );
+    }
+
+
+}
---
+++ conf/route.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * route config
+ +-----------------------------------------
+ * @author      page7 <zhounan0120@gmail.com>
+ * @version     $Id$
+ */
+
+return array(
+    'routes' => array(
+        /*
+        '/user/info/' => array(                 // key is a pathinfo rule, must have '/' at begin and end, eg : http://www.xxxx.com/user/info/data/update
+            'params'    => '$model/$method',    // use $ define params, value will insert into $_GET, eg: $_GET['model'] = 'data'; $_GET['method'] = 'update';
+            'callback'  => 'method::load',      // callback is a function or method
+        ),
+        */
+    ),
+);
